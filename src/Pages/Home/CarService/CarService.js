a147dbf3a94a005a36b773b91f713216aa5f8e5f
humayonitbd <humayonforid44@gmail.com>
2022-11-08 20:38:40 +0600

    home section finish

--- src/Pages/Home/CarService/CarService.js
+++ src/Pages/Home/CarService/CarService.js
@@ -1,32 +1,37 @@
 import React from 'react';
-import carImg1 from '../../../images/home-section-img/images.jpg'
-import carImg2 from '../../../images/home-section-img/images-_1_.jpg'
-import carImg3 from '../../../images/home-section-img/images-_2_.jpg'
-import carImg4 from '../../../images/home-section-img/images-_3_.jpg'
+import icon1 from '../../../images/home-section-img/icon1.png'
+import icon2 from '../../../images/home-section-img/icon2.png'
+import icon3 from '../../../images/home-section-img/icon3.png'
+
 import './CarService.css'
 
 const CarService = () => {
     return (
-        <div className='bg-black py-10 my-20'>
-           <h3 className='text-white text-center text-3xl my-6'>Relative services</h3>
-            <div className='grid mx-10 grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-6'>
-            <div className='serviceImg1 text-center'>
-                <img src={carImg1} alt="" />
-                <h2 className='service-title'>service1</h2>
+        <div className='grid md:mx-10 lg:mx-10 grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-10 my-36'>
+            <div className='flex justify-center items-center'>
+                <h2 className='text-3xl'>
+             <span className='text-red-500 font-bold text-3xl md:text-7xl lg:text-8xl'> 100% </span>Satisfaction Guarantee
+                </h2>
             </div>
-            <div className='serviceImg1'>
-                <img src={carImg2} alt="" />
-                <h2 className='service-title'>service2</h2>
+
+            <div className=' lg:text-start text-center '>
+            <div className='flex lg:justify-start md:justify-start justify-center items-center'> <img className='' src={icon1} alt="" /></div>
+                <h1 className='text-2xl py-2 font-bold'>Quality Support</h1>
+                <p>our Repair Services offers quality help programs for any vehicles that permit them to consistently.</p>
             </div>
-            <div className='serviceImg1'>
-                <img src={carImg3} alt="" />
-                <h2 className='service-title'>service3</h2>
+
+            <div className=' lg:text-start text-center'>
+           <div className='flex lg:justify-start md:justify-start justify-center items-center'> <img className='' src={icon2} alt="" /></div>
+                <h1 className='text-2xl py-2 font-bold'>All Car Makes</h1>
+                <p>our Repair Services offers quality help programs for any vehicles that permit them to consistently.</p>
+
             </div>
-            <div className='serviceImg1'>
-                <img src={carImg4} alt="" />
-                <h2 className='service-title'>service4</h2>
+            <div className='lg:text-start text-center'>
+            <div className='flex lg:justify-start md:justify-start justify-center items-center'> <img className='' src={icon3} alt="" /></div>
+                <h1 className='text-2xl py-2 font-bold'>All Car Makes</h1>
+                <p>our Repair Services offers quality help programs for any vehicles that permit them to consistently.</p>
             </div>
-        </div>
+           
         </div>
     );
 };
--- src/Pages/Home/OthersSec/OthersSec.js
+++ src/Pages/Home/OthersSec/OthersSec.js
@@ -3,15 +3,17 @@ import aboutImg from '../../../images/about section.jpg';
 
 const OthersSec = () => {
     return (
-        <div className='my-20 mx-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10'>
+        <div className='bg-black text-white py-8'>
+            <div className='my-20 mx-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10'>
             <div>
-                <h2 className='font-bold text-2xl'>WHO WE ARE</h2>
-                <p className='text-2xl font-semibold my-5'>Professional Auto washing and detailing services</p>
-                <p className='text-lg'>Over the past 6 years we cleaned over 34,000 cars, saved over 8.9 million liters of water from being used in car washing and employed 50 youth. Our legacy is in the youth that gained work and life skills, grew their confidence and have moved on to bigger.</p>
+                <h2 className='font-bold text-2xl'>WELCOME TO OUR WORKSHOP</h2>
+                <p className='text-2xl font-semibold my-5'>Committed to Give High Quality Repairing & Services</p>
+                <p className='text-lg'>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps</p>
             </div>
-            <div>
-                <img src={aboutImg} alt="aboute img" />
+            <div className=''>
+                <img className='rounded-3xl' src={aboutImg} alt="aboute img" />
             </div>
+        </div>
         </div>
     );
 };
