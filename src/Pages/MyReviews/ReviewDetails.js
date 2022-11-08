fde627de54d1a4e4c399e674f6e602af283c4161
humayonitbd <humayonforid44@gmail.com>
2022-11-08 12:59:28 +0600

    simple responsive

--- src/Pages/MyReviews/ReviewDetails.js
+++ src/Pages/MyReviews/ReviewDetails.js
@@ -6,13 +6,13 @@ const ReviewDetails = ({reviews}) => {
     const {user} = useContext(AuthContext)
     const {name, review, rating, _id, serviceId, dateTime} = reviews;
     return (
-        <div className='w-8/12 mb-5  mx-auto'>
-            <div className='border p-5 flex justify-between items-center border-black rounded'>
-                <div className=''>
+        <div className='lg:w-8/12 md:w-8/12 mb-5 mx-2 lg:mx-auto md:mx-auto'>
+            <div className='border p-5 lg:flex md:flex justify-between items-center border-black rounded'>
+                <div className='w-80'>
                     <div className='flex mb-3 justify-start items-center'>
-                    {user?.email && user?.photoURL ? <img src={user?.photoURL} className='w-16 h-16 rounded-full' alt="" />: <img src={reviewUser} className='w-16 h-16 rounded-full' alt="" />}
+                    {user?.email && user?.photoURL ? <img src={user?.photoURL} className='w-16 h-16 mr-2 rounded-full' alt="" />: <img src={reviewUser} className='w-16 mr-2 h-16 rounded-full' alt="" />}
                     <div>
-                    <h3>{name}</h3>
+                    <h3 className='font-bold'>{name}</h3>
                     <p>{dateTime}</p>
                     <p>rating: {rating}</p>
                     </div>
@@ -22,7 +22,7 @@ const ReviewDetails = ({reviews}) => {
                     </div>
                 </div>
                 
-                <div>
+                <div className='lg:text-end text-start md:text-end w-50'>
                     <button className='btn bg-red-600 mr-5'>Delete</button>
                     <button className='btn btn-primary'>Update</button>
                 </div>
